33301ac1cced4007f2b2e43b868ba135ac03ef0b
mikaben89 <mika.89@hotmail.fr>
2014-10-22 12:00:49 +0200

    Model Gear

---
+++ gearLandingProject/src/Model/Gear.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Gear {
+	
+	private enum GearStatus{up, down, goingUp, goingDown, stuck}
+	private GearStatus status;
+	
+	public Gear(){
+		this.status = GearStatus.down;
+	}
+	
+	public GearStatus getStatus(){
+		return this.status;
+	}
+	
+	public void setStatus(GearStatus gearStatus){
+		this.status = gearStatus;
+	}	
+
+}
