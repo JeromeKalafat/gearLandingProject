f8e6e1b186514b59dc9fcc5e46328db27663689d
pedelattre <pedelattre@gmail.com>
2014-10-24 10:32:43 +0200

    Graphic Components added

--- gearLandingProject/src/View/ControlPanel.java
+++ gearLandingProject/src/View/ControlPanel.java
@@ -1,59 +1,187 @@
 package View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
+
+
 
 public class ControlPanel extends JPanel{
-	private JButton commandeButton;
-	private JLabel ecranLabel;
-	private JLabel situationTrainLabel;
-	private JLabel positionCommandeLabel;
+	
+	private Box commandBox;
+	private JPanel ledsPanel;
+	private BufferedImage leverImage;
+	private JLabel leverLabel;
+	private BufferedImage caseImage;
+	private JLabel caseLabel;
+	private BufferedImage screenImage;
+	private JLabel screenLabel;
+	private BufferedImage greenLedImage;
+	private JLabel greenLedLabel;
+	private BufferedImage orangeLedImage;
+	private JLabel orangeLedLabel;
+	private BufferedImage redLedImage;
+	private JLabel redLedLabel;
+	
 	
 	public ControlPanel(){
-		//PANNEAU GAUCHE
-		JPanel panneauGauche = new JPanel();
-		TitledBorder title;
-		title = BorderFactory.createTitledBorder("Position Train Atterrissage");
-		ecranLabel = new JLabel();
-		ecranLabel.setText("Sorti");
-		panneauGauche.setPreferredSize(new Dimension(300,370));
+		prepareInterface();
+	}
+
+	private void prepareInterface(){
 		
-		ecranLabel.setHorizontalAlignment(JLabel.CENTER);
-		ecranLabel.setPreferredSize(new Dimension(290,390));
+		//CONTROL PANEL - MAIN PANE
+		GridBagLayout grid = new GridBagLayout();
+		this.setSize(1000,800);
+		this.setLayout(grid);
 		
-		panneauGauche.setBorder(title);
-		panneauGauche.add(ecranLabel);
-		
-		//PANNEAU DROIT
-		JPanel panneauDroite = new JPanel();
-		
-		commandeButton = new JButton("COMMANDE");
-		situationTrainLabel = new JLabel();
-		situationTrainLabel.setText("Tout OK");
-		positionCommandeLabel = new JLabel();
-		positionCommandeLabel.setText("Basse");
-		panneauDroite.setPreferredSize(new Dimension(100,400));
-		
-		commandeButton.setPreferredSize(new Dimension(90,90));
-		
-		panneauDroite.setLayout(new BorderLayout());
-		panneauDroite.add(situationTrainLabel, BorderLayout.NORTH);
-		panneauDroite.add(positionCommandeLabel, BorderLayout.CENTER);
-		panneauDroite.add(commandeButton, BorderLayout.SOUTH);
+		//INIT CONSTRAINTS - gbc
+		GridBagConstraints gbc = new GridBagConstraints();
 		
 		
-		//ON AJOUTE LES DEUX PANNEAUX AU PANNEAU PRINCIPAL
-		this.setLayout(new BorderLayout());
-		this.add(panneauDroite, BorderLayout.EAST);
-		this.add(panneauGauche, BorderLayout.WEST);
+		
+		/*LEDS PANEL
+		 * 
+		 */
+		
+		ledsPanel = new JPanel();
+		GridBagLayout ledsGrid = new GridBagLayout();
+		ledsPanel.setPreferredSize(new Dimension(400,100));
+		ledsPanel.setLayout(ledsGrid);
+
+		//INIT CONSTRAINTS - ledsGbc
+		GridBagConstraints ledsGbc = new GridBagConstraints();
+		
+		//GREEN LED LABEL
+		try {
+			greenLedImage = ImageIO.read(new File("images/greenOff2.png"));
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		greenLedLabel = new JLabel(new ImageIcon(greenLedImage));
+		ledsGbc.gridx = 0; 
+		ledsGbc.gridy = 0;
+		ledsGbc.gridheight = 1;
+		ledsGbc.weightx = 1;
+		ledsGbc.anchor = GridBagConstraints.LINE_START;
+		ledsPanel.add(greenLedLabel, ledsGbc);
+				
+				
+		//ORANGE LED LABEL
+		try {
+			orangeLedImage = ImageIO.read(new File("images/orangeOff2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		orangeLedLabel = new JLabel(new ImageIcon(orangeLedImage));
+		ledsGbc.gridx = 1; 
+		ledsGbc.gridy = 0;
+		ledsGbc.gridheight = 1;
+		ledsGbc.weightx = 1;
+		ledsGbc.anchor = GridBagConstraints.CENTER;
+		ledsPanel.add(orangeLedLabel, ledsGbc);
+
+				
+		//RED LED LABEL
+		try {
+			redLedImage = ImageIO.read(new File("images/redOff2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		redLedLabel = new JLabel(new ImageIcon(redLedImage));
+		ledsGbc.gridx = 2; 
+		ledsGbc.gridy = 0;
+		ledsGbc.gridheight = 1;
+		ledsGbc.gridwidth = 1;
+		ledsGbc.weightx = 1;
+		ledsGbc.anchor = GridBagConstraints.LINE_END;
+		ledsPanel.add(redLedLabel, ledsGbc);
+		
+		
+		//ADDS LEDS PANEL TO THE MAIN PANEL WITH CONSTRAINTS
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		gbc.gridheight = 1;
+		gbc.weightx = 1;
+		gbc.anchor = GridBagConstraints.CENTER;
+		this.add(ledsPanel);
+		
+		/*LEDS PANEL - END
+		 * 
+		 */
+		
+		
+		
+		
+		/*SCREEN PANEL
+		 * 
+		 */
+		
+		//SCREEN - LEFT PANEL
+		try {
+			screenImage = ImageIO.read(new File("images/gearOut2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		screenLabel = new JLabel(new ImageIcon(screenImage));
+		screenLabel.setHorizontalAlignment(JLabel.LEFT);
+		
+		
+		//ADDS SCREEN LABEL TO THE MAIN PANEL WITH CONSTRAINTS
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		this.add(screenLabel, gbc);
+		
+		/*SCREEN PANEL - END
+		 * 
+		 */
+			
+		
+		
+		/*COMMAND BOX
+		 * 
+		 */
+		
+		//COMMAND BOX - USED TO STACK TWO SHAPES
+		commandBox = Box.createVerticalBox();
+		try {
+			leverImage = ImageIO.read(new File("images/lever2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		leverLabel = new JLabel(new ImageIcon(leverImage));
+		leverLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		commandBox.add(leverLabel);
+		
+		try {
+			caseImage = ImageIO.read(new File("images/leverCase2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		caseLabel = new JLabel(new ImageIcon(caseImage));
+		caseLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		commandBox.add(caseLabel);
+		
+		
+		//ADDS COMMAND BOX TO THE MAIN PANEL WITH CONSTRAINTS
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		this.add(commandBox, gbc);
+		
+		
+		/*COMMAND BOX - END
+		 * 
+		 */
+	
 	}
 
 }
